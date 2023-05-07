fdf9b764700e21ab94df1d27bfc3694025fff3a0
suman <sumanpok9@gmail.com>
2023-05-07 22:15:17 +0545

    Dummy code added for the merge conflict

--- area51.js
+++ area51.js
@@ -1,9 +1,14 @@
 // this is a confidential information not to be shared or be put in the public eyes
-const areaFiftyOne = () =>
-  "Highly confidential information. There are aliens that are vigorous but are they lethal!!!";
-const shouldYouGoThere = () => {
-  const hello = "hello";
-  console.log(hello);
+//@ts-check
+const areaFiftyOne = (dummyO) => {
+  const { lengthNumber, breadthNumber } = dummyO;
+
+  const hello =
+    "Highly confidential information. There are aliens that are vigorous but are they lethal!!!";
+  const shouldYouGoThere = () => {
+    const hello = "hello";
+    console.log(hello);
+  };
   const newFunction = () => {
     return "this newfunciton is used for clousre and therefore later to be called";
   };
@@ -14,7 +19,19 @@ const shouldYouGoThere = () => {
 
   // changes made by the admin
 
-  const calculateTheArea = () => {};
+  const calculateTheArea = () => {
+    if (lengthNumber === breadthNumber) return; // this is so that we dont get any squre
+    return (lengthNumber * breadthNumber) / 2;
+  };
 
-  const calculateMathForBall = () => {};
+  const calculateMathForBall = () => {
+    return "just some dummy data";
+  };
 };
+
+const dummyObj = {
+  lengthNumber: 5,
+  breadthNumber: 6,
+};
+
+areaFiftyOne(dummyObj);
