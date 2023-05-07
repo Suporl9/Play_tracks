c4d68025a4e17de0d2f27761066f59484941c6d3
suman <sumanpok9@gmail.com>
2023-05-07 16:05:57 +0545

    bypass staging for new files

--- newComponent.js
+++ newComponent.js
@@ -3,3 +3,4 @@
 export const headerContent = () => "Play Tracks";
 
 // Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam cupiditate dolores facere? Incidunt nostrum sint soluta illum id rerum laborum, voluptate explicabo nesciunt consequatur, quo animi et ipsum suscipit commodi!
+// Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut consequuntur provident, vitae soluta accusamus fugit excepturi eligendi, qui vero blanditiis est. Molestias perferendis laborum vero a nulla alias sequi necessitatibus.
