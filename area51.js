cba76d8635769df5eebb197acce99c1b139cf700
suman <sumanpok9@gmail.com>
2023-05-07 19:17:05 +0545

    cd

--- area51.js
+++ area51.js
@@ -1,3 +1,3 @@
 // this is a confidential information not to be shared or be put in the public eyes
 const areaFiftyOne = () =>
-  "Highly confidential information. There are animals inside area 51 !!!";
+  "Highly confidential information. There are animals inside area !!!";
