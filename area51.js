956b7dd066377144310523870d798333d78726a5
suman <sumanpok9@gmail.com>
2023-05-07 22:06:09 +0545

    testing some new merging conflicts

--- area51.js
+++ area51.js
@@ -1,16 +1,32 @@
 // this is a confidential information not to be shared or be put in the public eyes
 const areaFiftyOne = () =>
   "Highly confidential information. There are aliens that are vigorous but are they lethal!!!";
-const shouldYouGoThere = () => {
-  const hello = "hello";
-  console.log(hello);
+const shouldYouGoThere = (lengthNumber, breadthNumber) => {
+  const a = lengthNumber;
+  const b = breadthNumber;
+
+  if (a < 5 && b < 5) {
+    return;
+  }
+
+  const hello = "hello one two three";
+  console.log(hello + "there this is a test");
   const newFunction = () => {
-    return "this newfunciton is used for clousre and therefore later to be called";
+    return "this newfunciton is used for closure and therefore later to be called! some edits is done by me for the newFucntion";
   };
 
   const newMain = () => {
     return "this is the fucntion that i is not reliable so to be deleted when pushing to the production";
   };
 
-  const calculateMathForBall = () => {};
+  const calculateMathForBall = () => {
+    return a * b;
+  };
+
+  const addForTheArea = () => {
+    const text = "this is the new area for the circumference of the ball";
+    return a + b + text;
+  };
 };
+
+console.log(areaFiftyOne(6, 6));
