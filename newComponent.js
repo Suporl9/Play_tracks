a444c0da5e252fbc3c9cdb59e10c89435e8fddd2
suman <sumanpok9@gmail.com>
2023-05-07 16:00:57 +0545

    bypass staging area and commit

--- newComponent.js
+++ newComponent.js
@@ -1,8 +1,5 @@
 // @ts-check
 
-// Lorem
-
-// ipsum
-// dolor sit amet consectetur adipisicing elit. Nostrum vitae sit harum dolor quae illum modi tenetur qui laboriosam! Cumque consequuntur ad officia ipsam! Velit vitae ab expedita ipsam sapiente!
-
 export const headerContent = () => "Play Tracks";
+
+// Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam cupiditate dolores facere? Incidunt nostrum sint soluta illum id rerum laborum, voluptate explicabo nesciunt consequatur, quo animi et ipsum suscipit commodi!
