c40e574bcb1b0fc3ce9be9b2964a5ad79bfe3d45
suman <sumanpok9@gmail.com>
2023-05-07 21:29:36 +0545

    changes text to the branch main

--- area51.js
+++ area51.js
@@ -1,3 +1,7 @@
 // this is a confidential information not to be shared or be put in the public eyes
 const areaFiftyOne = () =>
-  "Highly confidential information. There are aliens that are vigorous !!!";
+  "Highly confidential information. There are aliens that are vigorous but are they lethal!!!";
+const shouldYouGoThere = () => {
+  const hello = "hello";
+  console.log(hello);
+};
